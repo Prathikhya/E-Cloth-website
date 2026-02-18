5cb2b8387b9567cdcf5db148e0e31dc6c50e26e5
prathikhya <prathikhyadevi25@gmail.com>
2026-02-18 19:56:56 +0530

    client part updated

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,12 +1,15 @@
-import './App.css'
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
 
 
 function App() {
-  
+  const [count, setCount] = useState(0)
+
   return (
     <>
-      <h1>BTS </h1>
-      
+    <h1>BTS</h1>
+    <p>it is 18 feb jhope birthday</p>
     </>
   )
 }
